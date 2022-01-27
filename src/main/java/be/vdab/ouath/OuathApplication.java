46b2c34122c73d1ed930be85691529add215ec68
Muschke <mussche.yannick@hotmail.be>
2022-01-27 14:13:01 +0100

    first commit

---
+++ src/main/java/be/vdab/ouath/OuathApplication.java
@@ -0,0 +1,13 @@
+package be.vdab.ouath;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OuathApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OuathApplication.class, args);
+    }
+
+}
---
+++ src/test/java/be/vdab/ouath/OuathApplicationTests.java
@@ -0,0 +1,13 @@
+package be.vdab.ouath;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OuathApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
